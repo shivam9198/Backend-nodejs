f6c5ad07d24cd774730653626bb0dec87a93b286
shivam gupta <shivamkashyap9198@gmail.com>
2025-01-15 01:59:21 +0530

    deleted user2.js

--- src/models/user2.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const user2Schema = new mongoose.Schema({
-    firstName: "string",
-    lastName: "string",
-    gender : "string",
-    age: "string",
-});
-
-module.exports = mongoose.model("User2",user2Schema);
